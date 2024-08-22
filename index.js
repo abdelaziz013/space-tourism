1c260a704a497f2f657e3fa6d1554b8bc9615937
Abdelaziz <abdelazizt320@gmail.com>
2024-08-22 08:18:35 +0300

    home page completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menuButton = document.getElementById('btnHamburger');
 const closeButton = document.getElementById('close');
-const mobileMenu = document.querySelector('.mobile__nav');
+const mobileMenu = document.querySelector('.nav');
 closeButton.addEventListener('click',()=>{
   mobileMenu.classList.add('hide')
   mobileMenu.classList.remove('show')
