9960255d2708332f6b5f08095b91bdad310e2dc0
Abdelaziz <abdelazizt320@gmail.com>
2024-11-06 10:38:57 +0200

    improve bg class

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,30 +1,15 @@
 import React from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
-
 const RootLayout = () => {
-
-  const {pathname}=useLocation()
-  // const backgroundClass=pathname.substring(1)?pathname.substring(1):'home'
-  const [bgClass,setBgClass]=React.useState('home')
-React.useEffect(()=>{
-  const path=pathname.substring(1)
-setBgClass(()=>path?path:'home')
-
-},[pathname])
-
-
-
+  const { pathname } = useLocation()
   return (
     <>
-    <div className={`layout layout__${bgClass}`}>
-    <Navbar />
-    <Outlet/>
-    </div>
-
+      <div className={`layout layout__${pathname.substring(1)?pathname.substring(1):'home'}`}>
+        <Navbar />
+        <Outlet />
+      </div>
     </>
-
   )
 }
-
 export default RootLayout;
