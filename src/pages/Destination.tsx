fec83ecb27427aabeb83765542d9f64816c40d15
Abdelaziz <abdelazizt320@gmail.com>
2024-11-06 21:13:44 +0200

    dest page

--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -1,9 +1,46 @@
 import React from 'react'
-
+import { DESTDATA } from '../data';
+import { getImageUrl } from '../utils/function';
 const Destination = () => {
+  const [destItem, setDestItem] = React.useState(DESTDATA[0])
+  const getDestItem = (index: number) => {
+    setDestItem(DESTDATA[index])
+  }
   return (
-    <div>Destination</div>
-  )
+    <div className="dest__content">
+      <p className="title"><span>01</span>Pick your destination</p>
+      <div className="container">
+        <div className="dest__content--img-wrapper">
+          <div className="dest__content__img--moon">
+            <div>
+              <img src={getImageUrl(destItem.images.png)} alt="moon-img" />
+            </div>
+          </div>
+        </div>
+        <div className="dest__content--desc-wrapper">
+          {/* dest nav */}
+          <ul className="dest_nav">
+            {DESTDATA.map((item, index) => (
+              <li key={index} onClick={() => getDestItem(index)} className={`dest_nav__list ${destItem.name === item.name ? "dest_nav__list--active" : ""}`}>{item.name}</li>
+            ))}
+          </ul>
+          <div className="dest__content__description">
+            <p className="txt-1 upper-case">{destItem.name}</p>
+            <p className="txt-9"> {destItem.description}.</p>
+          </div>
+          <div className="dest__content__description--dest-time">
+            <div className="dest__content__description--distance">
+              <p className="txt-7 upper-case"> Avg. distance</p>
+              <p className="txt-6 upper-case">{destItem.distance}</p>
+            </div>
+            <div className="dest__content__description--time">
+              <p className="txt-7 upper-case">  Est. travel time</p>
+              <p className="txt-6 upper-case"> {destItem.travel}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default Destination
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,29 +3,29 @@ import { NavLink } from "react-router-dom";
 // import Navbar from "../components/Navbar";
 const Home = () => {
   return (
-    <>
-       <div className="container home__content">
-      <div>
-        <p className="upper-case">
-          So, you want to travel to
-        </p>
-        <p className="txt-1 upper-case">
-          Space
-        </p>
-        <p className="txt-9">
-          Let’s face it; if you want to go to space, you might as well genuinely go to
-          outer space and not hover kind of on the edge of it. Well sit back, and relax
-          because we’ll give you a truly out of this world experience!
-        </p>
-      </div>
 
-      <div className="home__button flex flx-col fl-col-center">
-      <NavLink to="destination" className="btn">explore</NavLink>
+      <div className="container home__content">
+        <div>
+          <p className="upper-case">
+            So, you want to travel to
+          </p>
+          <p className="txt-1 upper-case">
+            Space
+          </p>
+          <p className="txt-9">
+            Let’s face it; if you want to go to space, you might as well genuinely go to
+            outer space and not hover kind of on the edge of it. Well sit back, and relax
+            because we’ll give you a truly out of this world experience!
+          </p>
+        </div>
 
-        {/* <a className="btn" href="destination-moon.html">explore</a> */}
+        <div className="home__button flex flx-col fl-col-center">
+          <NavLink to="destination" className="btn">explore</NavLink>
+
+          {/* <a className="btn" href="destination-moon.html">explore</a> */}
+        </div>
       </div>
-    </div>
-    </>
+   
   );
 };
 export default Home;
